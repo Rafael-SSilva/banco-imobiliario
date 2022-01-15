7e9a2b94ffdb04627563f365adf2d4b879d2cdfd
rafael.san <rafael.san@totvs.com.br>
2022-01-15 11:52:45 -0300

    add firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXAx9yaptlRfux3TnNfM1ehtppyGYWJSw",
+  authDomain: "monopoly-e24eb.firebaseapp.com",
+  databaseURL: "https://monopoly-e24eb-default-rtdb.firebaseio.com",
+  projectId: "monopoly-e24eb",
+  storageBucket: "monopoly-e24eb.appspot.com",
+  messagingSenderId: "955499301652",
+  appId: "1:955499301652:web:9360955b8ab8164d1b5001"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
